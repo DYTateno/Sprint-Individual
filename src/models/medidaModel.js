8f91cce4d180ff6ac96dcbdf735d88493309d80b
DYTateno <yudi.tateno@gmail.com>
2024-11-30 13:05:19 -0300

    js

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,16 +1,16 @@
 var database = require("../database/config");
 
 function buscarUltimasMedidas(idUser) {
-
-    var instrucaoSql = `SELECT momento, COUNT(id) as 'soma' FROM medida WHERE id >= ${idUser} GROUP BY momento`
-
+    var instrucaoSql = `SELECT momento, COUNT(id) as 'soma' FROM medida WHERE id >= ${idUser} GROUP BY momento ORDER BY momento DESC;
+    `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
 function buscarMedidasEmTempoReal(idUser) {
-
-    var instrucaoSql = `SELECT momento, COUNT(id) as 'soma' FROM medida WHERE id >= ${idUser} GROUP BY momento`
+    var instrucaoSql = `
+        SELECT momento, COUNT(id) as 'soma' FROM medida WHERE id >= ${idUser} GROUP BY momento ORDER BY momento DESC
+    `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
